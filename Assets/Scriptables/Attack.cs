ad693d952841b0318b2184be949e07d12bd8d058
PauFusco <99730051+PauFusco@users.noreply.github.com>
2025-05-06 15:17:11 +0200

    Editor tooltip
    - Added tool tips to show the purpose of the variables of the attacks in editor
    - Modified some player variables

--- Assets/Scriptables/Attack.cs
+++ Assets/Scriptables/Attack.cs
@@ -7,19 +7,33 @@ namespace PAttack
     [CreateAssetMenu(fileName = "New Attack", menuName = "Scriptable Object/Attack")]
     public class Attack : ScriptableObject
     {
-        [Header("Debug")]
+        [Tooltip("The name of the attack")]
         public string attackName;
 
         [Header("Frame Durations")]
-        public float anticipation; public float active; public float recovery;
+        [Tooltip("Number of frames in the attack's anticipation phase.")]
+        public float anticipation;
+        [Tooltip("Number of frames in the attack's active phase.")]
+        public float active;
+        [Tooltip("Number of frames in the attack's recovery phase.")]
+        public float recovery;
 
         [Header("HitBox")]
+        [Tooltip("A prefab with the hitbox that spawns in the attack's active phase.")]
         public UnityEngine.Object hitbox;
 
         [Header("Attack Input")]
+        [Tooltip("An array with the Input Reference Objects with the possible inputs for the attack.")]
         public CustomInputControl.InputReference[] input;
 
         [Header("Interactions")]
-        public bool airborne; public bool chargeable; public bool cancellable; public Attack[] cancellableInto;
+        [Tooltip("True if the attack will be executed while airborne.")]
+        public bool airborne;
+        [Tooltip("True if the attack can be charged by holding its button.")]
+        public bool chargeable;
+        [Tooltip("True if the attack can be cancelled into other attacks.")]
+        public bool cancellable;
+        [Tooltip("List of attacks it can be cancelled into.")]
+        public Attack[] cancellableInto;
     }
 }
