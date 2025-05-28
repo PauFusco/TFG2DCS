3c0cca0bb88d5ea3d13031990ad59e2363990915
PauFusco <99730051+PauFusco@users.noreply.github.com>
2025-05-28 13:16:31 +0200

    Changed names of Enemy States scripts for consistency

--- Assets/Scripts/Enemy/States/ES_Stagger.cs
+++ Assets/Scripts/Enemy/States/ES_Stagger.cs
@@ -2,26 +2,6 @@
 
 namespace EFSM
 {
-    public abstract class BaseState
-    {
-        protected AttackFSM parentFSM;
-        public EnemyBehaviour enemy;
-
-        public BaseState(AttackFSM parentFSM, EnemyBehaviour enemy)
-        {
-            this.parentFSM = parentFSM;
-            this.enemy = enemy;
-        }
-
-        public abstract void OnEnter();
-
-        public abstract void Update();
-
-        public abstract void FixedUpdate();
-
-        public abstract void OnExit();
-    }
-
     public class StaggerState : BaseState
     {
         private readonly float staggerFrames;
---
+++ Assets/Scripts/Enemy/States/E_BaseState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace EFSM
+{
+    public abstract class BaseState
+    {
+        protected AttackFSM parentFSM;
+        public EnemyBehaviour enemy;
+
+        public BaseState(AttackFSM parentFSM, EnemyBehaviour enemy)
+        {
+            this.parentFSM = parentFSM;
+            this.enemy = enemy;
+        }
+
+        public abstract void OnEnter();
+
+        public abstract void Update();
+
+        public abstract void FixedUpdate();
+
+        public abstract void OnExit();
+    }
+}
